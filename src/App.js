115e9bd8d096693b1a76556b40bd18df62604591
Buppi Khan <khanbuppi@gmail.com>
2023-02-15 21:14:11 -0500

    added header and navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, Header } from "./components";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Navbar />
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Navbar } from "./home/navbar/Navbar";
+export { default as Header } from "./home/header/Header";

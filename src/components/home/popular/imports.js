2e602ac94e29fecbfacc39c7fbc0a82666b985b7
Buppi Khan <khanbuppi@gmail.com>
2023-02-18 16:09:40 -0500

    added all the neccessary images for the later sections

---
+++ src/components/home/popular/imports.js
@@ -0,0 +1,6 @@
+import lovers_nightmare from "../../../assets/popimages/lovers_nightmare.png";
+import planet_discover from "../../../assets/popimages/planet_discover.png";
+import star_hunter from "../../../assets/popimages/star_hunter.png";
+import survivor from "../../../assets/popimages/survivor.png";
+
+export { lovers_nightmare, planet_discover, star_hunter, survivor };
---
+++ src/components/home/trending/imports.js
@@ -0,0 +1,6 @@
+import pool_deeppool from "../../../assets/trendimages/pool_deeppool.png";
+import pool_magicallake from "../../../assets/trendimages/pool_magicallake.png";
+import pool_nemesis from "../../../assets/trendimages/pool_nemesis.png";
+import pool_survivors from "../../../assets/trendimages/pool_survivors.png";
+
+export { pool_deeppool, pool_magicallake, pool_nemesis, pool_survivors };

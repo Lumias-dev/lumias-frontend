cd9681f1ac8ac615268343c6754d06e0482691ed
Buppi Khan <khanbuppi@gmail.com>
2023-02-17 22:38:05 -0500

    updated navbar, logo need some resolution work

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import "./header.css";
-import mainlogo32x32 from "../../assets/mainlogo32x32.png";
 
 const Header = () => {
   return (
     <div>
-      <div>
-        <h1>Hi</h1>
-      </div>
-      <div className="header-image">
-        <img src={mainlogo32x32} alt="mainlogo32x32" />
-      </div>
+      <div>header</div>
     </div>
   );
 };
--- src/components/home/navbar/Navbar.jsx
+++ src/components/home/navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./navbar.css";
+import mainlogo32x32 from "../../../assets/mainlogo32x32.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div>
         <div className="navbar-links">
-          <div className="navbar-links-logo">
+          <div className="header-image">
+            <img src={mainlogo32x32} alt="mainlogo32x32" />
+          </div>
+          <div className="navbar-title">
             <h1>LUMIAS</h1>
           </div>
           <div className="navbar-links-container">
             {/* create router */}
             <p>HOME</p>
-            <p>ABOUT US</p>
-            <p>DOCS</p>
             <p>NEWS</p>
+            <p>DOCS</p>
+            <p>ABOUT US</p>
           </div>
         </div>
       </div>

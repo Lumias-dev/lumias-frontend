97e446e3252b064c3ac0898a00399263c848b1b5
Buppi Khan <khanbuppi@gmail.com>
2023-02-19 18:20:45 -0500

    added navbar menu for smaller screens, need more edits

--- src/components/home/navbar/Navbar.jsx
+++ src/components/home/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./navbar.css";
-import mainlogo from "../../../assets/mainlogo.png";
+import { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import mainlogo32x32 from "../../../assets/mainlogo32x32.png";
 
+const Menu = () => (
+  <>
+    {/* create router */}
+    <p>HOME</p>
+    <p>NEWS</p>
+    <p>DOCS</p>
+    <p>ABOUT US</p>
+  </>
+);
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="navbar">
       <div className="navbar-links">
@@ -11,16 +23,32 @@ const Navbar = () => {
           {/* <img src={mainlogo} alt="mainlogo" /> */}
           <img src={mainlogo32x32} alt="mainlogo" />
         </div>
-        <div className="navbar-links-title">
-          <h1>LUMIAS</h1>
-        </div>
-        <div className="navbar-links-container">
-          {/* create router */}
-          <p>HOME</p>
-          <p>NEWS</p>
-          <p>DOCS</p>
-          <p>ABOUT US</p>
-        </div>
+      </div>
+      <div className="navbar-title">
+        <h1>LUMIAS</h1>
+      </div>
+      <div className="navbar-container">
+        <Menu />
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={23}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={23}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-links-menu-container scale-up-center">
+            <Menu />
+          </div>
+        )}
       </div>
     </div>
   );

352e3f4dd94d328317cccfb7ef7fa1cd8411c12e
Buppi Khan <khanbuppi@gmail.com>
2023-02-20 16:27:35 -0500

    replaced png with svgs, tweaked updates

--- src/components/home/updates/Updates.jsx
+++ src/components/home/updates/Updates.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./updates.css";
-import updates1 from "../../../assets/updates1.png";
-import updates2 from "../../../assets/updates2.png";
+import updates1 from "../../../assets/updates1.svg";
+import updates2 from "../../../assets/updates2.svg";
 
 const Updates = () => {
   return (
@@ -9,11 +9,11 @@ const Updates = () => {
       <h1>Updates!</h1>
       <p>Please update your client to 2.0.1 to enjoy these features</p>
       <div className="updates-explore">
-        <p>Explore</p>
+        <button type="">Explore</button>
       </div>
       <div className="updates-container">
-        <img src={updates1} height="300" width="450" alt="left image" />
-        <img src={updates2} height="300" width="450" alt="right image" />
+        <img src={updates1} alt="left image" />
+        <img src={updates2} alt="right image" />
         <div className="updates-container-statement">
           <p>One chain friends can now share achievements</p>
           <p>New staking pools for deifi nerds</p>

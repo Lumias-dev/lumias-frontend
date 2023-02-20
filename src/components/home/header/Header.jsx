a8e85989919f753a69fa2967db2eab01c4f90595
Buppi Khan <khanbuppi@gmail.com>
2023-02-19 19:53:38 -0500

    updated updates section

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import "./header.css";
 import defi_planet from "../../../assets/defi_planet.svg";
 import gamefi_planet from "../../../assets/gamefi_planet.svg";
-import bcgg from "../../../assets/bcgg.svg";
-import mainbg from "../../../assets/mainbg.svg";
-import Rectangle140 from "../../../assets/Rectangle140.svg";
 
 const Header = () => {
   return (
     <div className="header">
-      {/* <img src={bcgg} alt="background" /> */}
       <div className="header-image">
         <img src={defi_planet} alt="blue-planet" />
         <img src={gamefi_planet} alt="gamefi_planet" />
-        {/* <img src={bcgg} alt="bcgg" /> */}
-        {/* <img src={mainbg} alt="mainbg" /> */}
       </div>
     </div>
   );
--- src/components/home/updates/Updates.jsx
+++ src/components/home/updates/Updates.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./updates.css";
+import updates1 from "../../../assets/updates1.png";
+import updates2 from "../../../assets/updates2.png";
 
 const Updates = () => {
   return (
-    <div>
-      <div>Updates</div>
+    <div className="updates">
+      <h1>Updates!</h1>
+      <p>Please update your client to 2.0.1 to enjoy these features</p>
+      <div className="updates-explore">
+        <p>Explore</p>
+      </div>
+      <div className="updates-container">
+        <img src={updates1} height="300" width="450" alt="left image" />
+        <img src={updates2} height="300" width="450" alt="right image" />
+        <div className="updates-container-statement">
+          <p>One chain friends can now share achievements</p>
+          <p>New staking pools for deifi nerds</p>
+        </div>
+      </div>
     </div>
   );
 };

059dbd5b4a6ed38d0b4851a1db68e2abddb742eb
Buppi Khan <khanbuppi@gmail.com>
2023-03-05 17:28:02 -0500

    redesigned header planets

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import "./header.css";
 import defi_planet from "../../../assets/defi_planet.svg";
 import gamefi_planet from "../../../assets/gamefi_planet.svg";
+import galaxydust from "../../../assets/galaxydust.svg";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-image">
-        <img src={defi_planet} height="700" width="700" alt="blue-planet" />
-        <img src={gamefi_planet} height="800" width="740" alt="gamefi_planet" />
-        {/* <img src={defi_planet} alt="blue-planet" />
-        <img src={gamefi_planet} alt="gamefi_planet" /> */}
+        <img
+          src={gamefi_planet}
+          height="1000"
+          width="1000"
+          className="gamefi-planet"
+        />
+        <img
+          src={defi_planet}
+          height="800"
+          width="800"
+          className="defi-planet"
+        />
       </div>
     </div>
   );

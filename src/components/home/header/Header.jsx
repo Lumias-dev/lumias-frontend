d52bb5ded02311ae26423613ff86d92a805d9aac
Buppi Khan <khanbuppi@gmail.com>
2023-03-06 20:29:13 -0500

    added other images for header, need to fix visibility

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -3,6 +3,9 @@ import "./header.css";
 import defi_planet from "../../../assets/defi_planet.svg";
 import gamefi_planet from "../../../assets/gamefi_planet.svg";
 import galaxydust from "../../../assets/galaxydust.svg";
+import galaxydust2 from "../../../assets/galaxydust2.svg";
+import ast from "../../../assets/ast.svg";
+import aststring from "../../../assets/aststring.svg";
 
 const Header = () => {
   return (
@@ -20,6 +23,9 @@ const Header = () => {
           width="800"
           className="defi-planet"
         />
+        <img src={aststring} className="aststring" />
+        <img src={ast} className="ast" />
+        <img src={galaxydust2} className="galaxydust2" />
       </div>
     </div>
   );

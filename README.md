38499ce2ab34e4a415a9f2a0ac8086ab5e6d4df6
Buppi Khan <khanbuppi@gmail.com>
2023-03-12 15:52:17 -0400

    updated news page

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Getting Started with Create React App
 
 This is a prototype homepage of Lumias
+
+# How to deploy
+
+In terminal write
+
+```bash
+npm run build
+```
+
+This will produce a dist folder
+Drag everything in the dist folder to the hosting application
--- src/components/pages/News.jsx
+++ src/components/pages/News.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./news.css";
 import newslaunch from "../../assets/news/newslaunch.png";
-import newspolygon from "../../assets/news/newslaunch.png";
-import newswhitepaper from "../../assets/news/newslaunch.png";
+import newspolygon from "../../assets/news/newspolygon.png";
+import newswhitepaper from "../../assets/news/newswhitepaper.png";
 
-const Article = (imgurl, date, title) => {
-  return (
-    <div className="article">
-      <div className="article-images">
-        <img src={imgurl} alt="imgurl" />
-      </div>
-      <div className="article-content">
-        <p>{date}</p>
-        <p>{title}</p>
-      </div>
-    </div>
-  );
-};
+// const Article = (imgurl, date, title) => {
+//   return (
+//     <div className="article">
+//       <div className="article-images">
+//         <img src={imgurl} alt="imgurl" />
+//       </div>
+//       <div className="article-content">
+//         <p>{date}</p>
+//         <p>{title}</p>
+//       </div>
+//     </div>
+//   );
+// };
 
 const News = () => {
   return (
@@ -28,11 +28,38 @@ const News = () => {
       <div className="news-latest">
         <h1>Latest News</h1>
         <p>Stay up to date on the latest news</p>
-        {/* <Article
-          imgurl={newspolygon}
-          date="Sep 26th, 2023"
-          title="Lumias Launch"
-        /> */}
+        <div className="news-latest-container">
+          <div>
+            <a
+              href="https://lumias.medium.com/lumias-polygon-collaboration-5a1c2b4d82ac"
+              target="_blank"
+            >
+              <img src={newspolygon} alt="polygon" />
+              <h1>Lumias: Polygon Collaboration</h1>
+              <p>Oct 20, 2022</p>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://lumias.medium.com/lumias-whitepaper-a15f66aee125"
+              target="_blank"
+            >
+              <img src={newswhitepaper} alt="whitepaper" />
+              <h1>Lumias: Whitepaper</h1>
+              <p>Oct 9, 2022</p>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://lumias.medium.com/lumias-launch-2fcb3f0e7d9b"
+              target="_blank"
+            >
+              <img src={newslaunch} alt="launch" />
+              <h1>Lumias Launch</h1>
+              <p>Oct 9, 2022</p>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
